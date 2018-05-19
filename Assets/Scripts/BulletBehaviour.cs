d81ee3fb406ac4af72fbfa7f7fe6e2384be5153a
funkYzWo <angelkozlev@gmail.com>
2018-05-19 18:14:41 +0100

    0.3

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+
+    private float timer;
+    public int bulletLife = 4;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 1.0F * Time.deltaTime;
+
+        if (timer >= bulletLife)
+        {
+            GameObject.Destroy(gameObject);
+        }
+
+    }
+
+    void OnCollisionEnter()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
